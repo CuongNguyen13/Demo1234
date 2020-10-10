bdae609483f6e70ced06fd214667c09116a9848a
dang3tion <65795540+dang3tion@users.noreply.github.com>
2020-10-10 11:17:27 +0700

    chức năng 3
    mô tả

--- Animal/src/Animal.java
+++ Animal/src/Animal.java
@@ -3,5 +3,6 @@ public class Animal {
 	public static void main(String[] args) {
 		System.out.println("Hello Word");
 		System.out.println("Animmmmmmmmmm");
+		System.out.println("Animmmmmmmmmm");
 	}
 }
